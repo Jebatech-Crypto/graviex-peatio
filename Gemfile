22f18ad6ac156fca0c90707f2894b6be5cf29fc3
Jebatech-Crypto <51023977+Jebatech-Crypto@users.noreply.github.com>
2019-10-08 06:23:15 +0800

    Update 1/1

--- Gemfile
+++ Gemfile
@@ -48,7 +48,7 @@ gem 'coffee-rails'
 gem 'uglifier'
 gem "jquery-rails"
 gem "angularjs-rails"
-gem 'bootstrap-sass', '~> 3.2.0.2'
+gem 'bootstrap-sass', '~> 3.2.0.4'
 gem 'bootstrap-wysihtml5-rails'
 gem 'font-awesome-sass', '~> 4.5.0.0'
 gem 'bourbon'
--- app/models/worker/withdraw_coin.rb
+++ app/models/worker/withdraw_coin.rb
@@ -55,7 +55,8 @@ module Worker
 
             # CoinRPC[withdraw.currency].settxfee fee
             @amount = (withdraw.amount*100000000.0).round / 100000000.0
-            txid = CoinRPC[withdraw.currency].sendtoaddress withdraw.channel.currency_obj.base_secret, withdraw.fund_uid, @amount.to_f
+            txid = CoinRPC[withdraw.currency].sendtoaddress withdraw.fund_uid, @amount.to_f
+            #txid = CoinRPC[withdraw.currency].sendtoaddress withdraw.channel.currency_obj.base_secret, withdraw.fund_uid, @amount.to_f
           end
   
           withdraw.whodunnit('Worker::WithdrawCoin') do
