b24f1539a0879f4efea79d11cf69727828effe51
Jebatech-Crypto <51023977+Jebatech-Crypto@users.noreply.github.com>
2019-10-08 15:18:42 +0800

    Update

--- config/maintenance.sh
+++ config/maintenance.sh
@@ -2,13 +2,13 @@
 
 if [ "$1" == "off" ]; then
   echo "switch off maintenance mode"
-  ln -sf /home/graviex/graviex-exchange/config/nginx.conf /etc/nginx/conf.d/graviex.conf
-  mv /home/graviex/graviex-exchange/public/404.html.old /home/graviex/graviex-exchange/public/404.html
+  ln -sf /home/deploy/peatio/config/nginx.conf /etc/nginx/conf.d/graviex.conf
+  mv /home/deploy/peatio/public/404.html.old /home/deploy/peatio/public/404.html
   service nginx reload
 else
   echo "switch on maintenance mode"
-  ln -sf /home/graviex/graviex-exchange/config/nginx_maintenance.conf /etc/nginx/conf.d/graviex.conf
-  mv /home/graviex/graviex-exchange/public/404.html /home/graviex/graviex-exchange/public/404.html.old
-  cp /home/graviex/graviex-exchange/public/503.html /home/graviex/graviex-exchange/public/404.html
+  ln -sf /home/deploy/peatio/config/nginx_maintenance.conf /etc/nginx/conf.d/peatio.conf
+  mv /home/deploy/peatio/public/404.html /home/deploy/peatio/public/404.html.old
+  cp /home/deploy/peatio/public/503.html /home/deploy/peatio/public/404.html
   service nginx reload
 fi
